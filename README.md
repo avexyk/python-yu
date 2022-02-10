20da078430cbb9372fc1a70fe113ef45a0fa22fd
avexyk <avexyk@gmail.com>
2022-02-09 23:20:24 -0600

    add: archivos del día 1

---
+++ README.md
@@ -0,0 +1,2 @@
+# python-yu
+Bootcamp de Python
---
+++ day001/printing.py
@@ -0,0 +1,35 @@
+print('Something')
+
+print('Day 1 - Python Print Function')
+print('The function is declared like this:')
+print("print('what to print')")
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+name = input('What is your name?')
+print(len(name))
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+a_temp = a
+b_temp = b
+a = b_temp
+b = a_temp
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+print("Salto de línea\nSalto de línea\nSalto de línea")
+print("Francisco " + "Javier")
